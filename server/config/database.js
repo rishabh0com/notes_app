cfdccad9bf28f5e527fb6f0182f5bf0966839c81
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-29 09:49:47 +0530

    feat: modify mongoose and added passport with google auth in index

--- server/config/database.js
+++ server/config/database.js
@@ -2,6 +2,12 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-const connection = mongoose.connect(process.env.MONGO_URI);
+const connection = mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log("Error from Databse",err));
 
 module.exports = connection;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
+// client ID from google cloud
+// 143817650055-6d4sn9h6gvfkqp61qcglq6c38vf0q00r.apps.googleusercontent.com
+
 const express = require("express");
+const moment = require("moment");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const oAuth2Strategy = require("passport-google-oauth2").Strategy;
 const connection = require("./config/database");
 const noteRoutes = require("./src/routes/note.routes.js");
-const moment = require("moment");
 const userRoutes = require("./src/routes/user.routes.js");
-const cors = require("cors");
+const UserModel = require("./src/models/user.model.js");
 
 const app = express();
 const PORT = 4000;
+
+// google client Id and secret :
+const clientId =
+  "143817650055-6d4sn9h6gvfkqp61qcglq6c38vf0q00r.apps.googleusercontent.com";
+const clientSecret = "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";
 
 // middleware :
 app.use(express.json());
@@ -26,11 +38,71 @@ app.use(
     credentials: true,
   })
 );
+// setup session :
+app.use(
+  session({
+    secret: "sessionSecretforGoogleLogin",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-// routes
-app.get("/", (req, res) => {
-  res.send("This is the home page ~");
+// setup passport :
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new oAuth2Strategy(
+    {
+      clientID: clientId,
+      clientSecret: clientSecret,
+      callbackURL: "/auth/google/callback",
+      scope: ["email", "profile"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log("google profile: ", profile);
+      try {
+        let user = await UserModel.findOne({ googleId: profile.id });
+        if (!user) {
+          user = await UserModel({
+            googleId: profile.id,
+            displayName: profile.displayName,
+            email: profile.emails[0].value,
+            displayImage: profile.photos[0].value,
+          }).save();
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(error, null);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
 });
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// intialize google login :
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "http://localhost:5173/",
+    failureRedirect: "http://localhost:5173/login",
+  })
+);
+
 // notes routes :
 app.use("/notes", noteRoutes);
 //user routes :
@@ -41,7 +113,6 @@ app.listen(PORT, async () => {
   try {
     console.log("Server is running on port", PORT);
     await connection;
-    console.log("DB is connected!");
   } catch (error) {
     console.log("Error: ", error);
   }
