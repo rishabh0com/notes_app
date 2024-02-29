e62746d66135e2f646dd2be24c710417e2d903a6
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-29 09:48:35 +0530

    feat: modify usermodel according to google auth

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    googleId: {
+      type: String,
+      required: true,
+    },
+    displayName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    displayImage: {
+      type: String,
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const UserModel = mongoose.model("User", userSchema);
 
